cc90bf3139e3d1c345e7adc98429bcf4fdd671b9
sharmin74 <sharminoomatia205@gmail.com>
2025-01-29 16:30:50 -0500

    Adding my 1st component

---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Profile() {
+    return (
+        <div>
+            <h1>Sharmin Oomatia</h1>
+            <p>This is my 1st component</p>
+      </div>
+    )
+  }
+  
+
+export default Profile;
